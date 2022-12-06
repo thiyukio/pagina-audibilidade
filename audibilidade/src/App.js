fcc7b8b8c58074bf551fe93bc7960786d1d78601
Thiago Yukio Nagatomo <thiago.yukio@inspira.com.br>
2022-12-06 07:40:29 -0300

    scroll primeiro elemento

--- audibilidade/src/App.js
+++ audibilidade/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useRef } from 'react';
 
 function Button({url, onClick, children}) {
   const redirect = () => {
@@ -28,22 +29,64 @@ function Header() {
   )
 }
 
-function Hero() {
+function Hero({learnMore}) {
   return (
     <div className='hero-container'>
       <h1>Uma proposta para acessibilidade de deficientes auditivos</h1>
-      <Button> Saiba mais</Button>
+      <Button onClick={learnMore}> Saiba mais</Button>
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
     </div>
   )
 }
 
 function App() {
+  const firstElementRef = useRef(null);
+
+  const scrollToFirst = () => {
+    firstElementRef.current.scrollIntoView();
+  }
+
   return (
     <div className="App">
       <Header/>
-      <Hero/>
-      Exercitation aliqua laborum mollit esse nulla mollit adipisicing sit fugiat nisi magna ad ipsum quis. Sunt sit ea ullamco laborum. Ad veniam exercitation elit eiusmod proident laborum velit exercitation ad et fugiat. Quis ut nisi laborum irure officia reprehenderit officia pariatur laborum elit velit mollit. Sint labore magna ipsum ea non et quis aliqua veniam. Exercitation qui eu amet qui.
+      <Hero learnMore={scrollToFirst}/>
+      <div ref={firstElementRef}>
+        <p>
+          Exercitation aliqua laborum mollit esse nulla mollit adipisicing sit fugiat nisi magna ad ipsum quis. Sunt sit ea ullamco laborum. Ad veniam exercitation elit eiusmod proident laborum velit exercitation ad et fugiat. Quis ut nisi laborum irure officia reprehenderit officia pariatur laborum elit velit mollit. Sint labore magna ipsum ea non et quis aliqua veniam. Exercitation qui eu amet qui.
+        </p>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <p>
+          Exercitation aliqua laborum mollit esse nulla mollit adipisicing sit fugiat nisi magna ad ipsum quis. Sunt sit ea ullamco laborum. Ad veniam exercitation elit eiusmod proident laborum velit exercitation ad et fugiat. Quis ut nisi laborum irure officia reprehenderit officia pariatur laborum elit velit mollit. Sint labore magna ipsum ea non et quis aliqua veniam. Exercitation qui eu amet qui.
+        </p>
+        <p>
+          Exercitation aliqua laborum mollit esse nulla mollit adipisicing sit fugiat nisi magna ad ipsum quis. Sunt sit ea ullamco laborum. Ad veniam exercitation elit eiusmod proident laborum velit exercitation ad et fugiat. Quis ut nisi laborum irure officia reprehenderit officia pariatur laborum elit velit mollit. Sint labore magna ipsum ea non et quis aliqua veniam. Exercitation qui eu amet qui.
+        </p>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
